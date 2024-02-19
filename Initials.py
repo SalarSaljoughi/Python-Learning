fcede412a23a38130ea2cad9ec4aab6aec0f7c5d
Salar Saljoughi <saljoughi.salar@gmail.com>
2024-02-18 22:38:27 -0700

    Add files via upload

---
+++ Initials.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # ASCII ART
+
+# writing my initialls in ASCII 
+
+# In[1]:
+
+
+# my name is Salar Saljoughi
+# I'm photographer and trying to learn coding
+print(" SSS     SSS ")
+print("S   S   S   S")
+print("S       S    ")
+print(" SSS     SSS ")
+print("     S        S")
+print("S   S   S   S")
+print(" SSS     SSS ")
+
+
+# Creating snowman in ASCII
+
+# In[ ]:
+
+
+
+
